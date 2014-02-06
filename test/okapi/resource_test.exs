d14041db8f0a3840d5e1c16159847e5cf61eef1b
Ray Morgan <ray@romotive.com>
2014-02-05 17:20:37 -0800

    Test formatting

--- test/okapi/resource_test.exs
+++ test/okapi/resource_test.exs
@@ -15,19 +15,14 @@ defmodule OkapiTest.Resource do
   end
 
   test "valid GET!" do
-    # {status_code, headers, charge} = Stripe.Charge.retrieve!(id: "ch_1tEa2FtVyao2Yb")
-
     charge = Stripe.Charge.retrieve!(id: "ch_1tEa2FtVyao2Yb")
-
     assert charge["id"] == "ch_1tEa2FtVyao2Yb"
   end
 
   test "valid POST" do
-    {:ok, {status_code, headers, charge}} = Stripe.Charge.create(
-      amount: 500,
-      currency: "USD",
-      customer: "cus_1u0czSrpdEB6EM"
-    )
+    input = [amount: 500, currency: "usd", customer: "cus_1u0czSrpdEB6EM"]
+
+    {:ok, {status_code, headers, charge}} = Stripe.Charge.create(input)
 
     assert status_code == 200
     assert is_list(headers) == true
@@ -42,11 +37,9 @@ defmodule OkapiTest.Resource do
   end
 
   test "valid POST!" do
-    charge = Stripe.Charge.create!(
-      amount: 500,
-      currency: "USD",
-      customer: "cus_1u0czSrpdEB6EM"
-    )
+    input = [amount: 500, currency: "usd", customer: "cus_1u0czSrpdEB6EM"]
+
+    charge = Stripe.Charge.create!(input)
 
     assert charge["id"] != nil
   end
